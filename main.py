912f518ef028dfa1cfdff73babca9618a1fc3f06
ADIB-the-NOOB <mdadib550@gmail.com>
2021-12-19 16:05:30 +0600

    Slow Scanning

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import sys
 import time
 
 usage = "python3 main.py TARGET START_PORT END_PORT"
-print()
-print("-"*70)
+print("-" * 70)
 
 if len(sys.argv) != 4:
     print(usage)
@@ -23,11 +22,13 @@ end_port = int(sys.argv[3])
 print("Scanning target:", target)
 
 for port in range(start_port, end_port + 1):
+    print("Scanning port:", port)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
     # sock_stream means TCP ports
     # AF_Inet means address family of Internet and IPv4
-    print("Scanning port:", port)
-    conn = s.connect((target, port))
+    print("Scanning target:", target)
+    conn = s.connect_ex((target, port))
     if not conn:
-        print("Port {} is closed".format(port))
+        print("Port {} is OPEN".format(port))
     s.close()
